079bc074d74b20f418bd5f9fc9cacd1349db28fe
KCH <doolysmile1@naver.com>
2022-10-18 22:33:16 +0900

    initial settings

---
+++ 1Week_Mission/src/test/java/com/ll/project/ebook/EbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.project.ebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
