70c27fe89922c8c7e858f7321802e0d24b3cb467
KCH <doolysmile1@naver.com>
2022-10-19 03:16:29 +0900

    feat : base entity 추가

--- 1Week_Mission/src/main/java/com/ll/project/ebook/EbookApplication.java
+++ 1Week_Mission/src/main/java/com/ll/project/ebook/EbookApplication.java
@@ -2,8 +2,10 @@ package com.ll.project.ebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EbookApplication {
 
 	public static void main(String[] args) {
